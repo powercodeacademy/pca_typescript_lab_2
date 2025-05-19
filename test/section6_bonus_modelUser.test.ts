32a4f9f6ec79507eebfc4f5c76f138708918af51
Mike <mclancy96@gmail.com>
2025-05-19 16:05:39 -0400

    Add explicit type to user

--- test/section6_bonus_modelUser.test.ts
+++ test/section6_bonus_modelUser.test.ts
@@ -3,16 +3,27 @@ import * as ts from "typescript";
 import { readFileSync } from "fs";
 import { join } from "path";
 import vm from "vm";
+import { expectVariableExplicitTypeAnnotation } from "chai_typescript_type_annotation_tests";
 
 describe("Lab 2 — Bonus: UserProfile", () => {
   let context: any = {};
+  const filePath = join(__dirname, "../src/section6_bonus_modelUser.ts");
 
   before(() => {
-    const filePath = join(__dirname, "../src/section6_bonus_modelUser.ts");
     const tsCode = readFileSync(filePath, "utf8");
     const jsCode = ts.transpile(tsCode);
     vm.createContext(context);
     vm.runInContext(jsCode, context);
+  });
+
+  it("should define a Product type with correct keys and types", () => {
+    const sourceCode = readFileSync(filePath, "utf8");
+    expect(sourceCode).to.include("type UserProfile");
+    expect(sourceCode).to.match(/type\s+UserProfile/);
+    expect(sourceCode).to.match(/id\s*:\s*number/);
+    expect(sourceCode).to.match(/username\s*:\s*string/);
+    expect(sourceCode).to.match(/email\s*:\s*string/);
+    expect(sourceCode).to.match(/createdAt\s*:\s*string/);
   });
 
   it("should define a userProfile object with required fields", () => {
@@ -29,4 +40,6 @@ describe("Lab 2 — Bonus: UserProfile", () => {
     if ("bio" in user) expect(user.bio).to.be.a("string");
     if ("avatarUrl" in user) expect(user.avatarUrl).to.be.a("string");
   });
+
+  expectVariableExplicitTypeAnnotation(filePath, "userProfile", "UserProfile");
 });
