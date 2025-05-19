1c150b79ea911eebdea22cce99c1e70c528898bc
Mike <mclancy96@gmail.com>
2025-05-19 15:51:10 -0400

    Add type testing for literal types

--- test/section4_literal_types.test.ts
+++ test/section4_literal_types.test.ts
@@ -3,12 +3,13 @@ import * as ts from "typescript";
 import { readFileSync } from "fs";
 import { join } from "path";
 import vm from "vm";
+import { expectVariableExplicitTypeAnnotation } from "chai_typescript_type_annotation_tests";
 
 describe("Lab 2 — Section 4: Literal Types", () => {
   let context: any = {};
+  const filePath = join(__dirname, "../src/section4_literal_types.ts");
 
   before(() => {
-    const filePath = join(__dirname, "../src/section4_literal_types.ts");
     const tsCode = readFileSync(filePath, "utf8");
     const jsCode = ts.transpile(tsCode);
     vm.createContext(context);
@@ -19,4 +20,10 @@ describe("Lab 2 — Section 4: Literal Types", () => {
     const validValues = ["loading", "success", "error"];
     expect(validValues).to.include(context.loadingStatus);
   });
+
+  expectVariableExplicitTypeAnnotation(
+    filePath,
+    "loadingStatus",
+    '"loading" | "success" | "error"'
+  );
 });
