be313f7d383b16fbdf6b3c46dd83b04d3e5ad98c
Emily Kanarek <emilyakanarek@gmail.com>
2025-09-15 09:28:46 -0400

    Calculate shopping cart total

--- src/section7_bonus_shoppingCart.ts
+++ src/section7_bonus_shoppingCart.ts
@@ -12,3 +12,18 @@
 //    - returns the total cost (sum of quantity × price for each item)
 
 // Your code here 👇
+type CartItem = {
+  productId: number;
+  name: string;
+  quantity: number;
+  price: number;
+};
+
+const cart: CartItem[] = [
+  { productId: 1, name: "Item1", quantity: 1, price: 10 },
+  { productId: 2, name: "Item2", quantity: 5, price: 23 },
+];
+
+const calculateTotal = (cart: CartItem[]): number => {
+  return cart.reduce((total, item) => total + item.quantity * item.price, 0);
+};
