92d71229dfea6bea8ea564849835121d3370c0fc
elizabethwilby <elizabethwilby@Elizabeths-MacBook-Pro.local>
2026-09-08 13:45:32 -0400

    finished lab

--- src/section5_type_alias.ts
+++ src/section5_type_alias.ts
@@ -1,3 +1,14 @@
 // Create a Product type alias and an exampleProduct variable
 
 // Your code here 👇
+ type Product = {
+  id: number
+  name: string
+  price: number
+ }
+
+ let exampleProduct: Product = {
+  id: 1,
+  name: "Computa",
+  price: 200.00
+ }
--- src/section7_bonus_shoppingCart.ts
+++ src/section7_bonus_shoppingCart.ts
@@ -1,3 +1,23 @@
 // Create CartItem type, cart array, and calculateTotal function
 
 // Your code here 👇
+
+type CartItem = {
+  productId: number
+  name: string
+  quantity: number
+  price: number
+}
+
+let cart: CartItem[] = [
+  { productId: 1, name: "Notebook", quantity: 2, price: 3.50 },
+  { productId: 2, name: "Pen", quantity: 4, price: 1.50 }
+]
+
+function calculateTotal(cart: CartItem[]): number {
+  let total: number = 0;
+  for (const item of cart) {
+    total = total + item.quantity * item.price;
+  }
+  return total
+}
