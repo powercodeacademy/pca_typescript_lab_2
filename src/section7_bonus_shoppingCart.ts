04b0a6546eb61f8072c36cf80b5216c2f6380453
+ <+>
2026-09-08 12:37:18 -0400

    Complete typescript lab 2

--- src/section7_bonus_shoppingCart.ts
+++ src/section7_bonus_shoppingCart.ts
@@ -1,3 +1,24 @@
 // Create CartItem type, cart array, and calculateTotal function
 
 // Your code here 👇
+
+type CartItem = {
+productId: number
+name: string
+price: number
+quantity: number
+}
+
+const cart: CartItem[] = [
+  { productId: 1, name: "oranges", price: 2, quantity: 2 },
+  { productId: 2, name: "apples", price: 5, quantity: 3 },
+]
+  
+
+function calculateTotal(cart: CartItem[]): number {
+  return cart.reduce((total, item) => {
+    return total + item.price * item.quantity
+  }, 0)
+}
+
+console.log(calculateTotal(cart))
